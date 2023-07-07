da41649e005960d2061d28d43bb94a355be4045e
Shivam-2326 <55495822+Shivam-2326@users.noreply.github.com>
2023-07-08 02:49:43 +0530

    Import issue fix

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home.routing';
-import { SummaryPipe } from 'src/app/pipes/summary.pipe';
+import { SummaryPipe } from './../../pipes/summary.pipe';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { MenuItemComponent } from 'src/app/components/menu-item/menu-item.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
-import { AppModule } from 'src/app/app.module';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
