a5758b0d4f8ac02bbbb392e1de8c4dd0d6e3582c
Shivam-2326 <55495822+Shivam-2326@users.noreply.github.com>
2023-07-08 02:53:20 +0530

    imports fixes

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home.routing';
 import { SummaryPipe } from './../../pipes/summary.pipe';
-import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
-import { MenuItemComponent } from 'src/app/components/menu-item/menu-item.component';
-import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { NavbarComponent } from './../../components/navbar/navbar.component';
+import { MenuItemComponent } from './../../components/menu-item/menu-item.component';
+import { FooterComponent } from './../../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
