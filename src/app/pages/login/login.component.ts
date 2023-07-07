44dcec8dbecc9b529b7cd770f1af7158ce8eebcb
Shivam-2326 <55495822+Shivam-2326@users.noreply.github.com>
2023-07-08 02:45:49 +0530

    Big fix stackbliz

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  providers: [AuthService],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
